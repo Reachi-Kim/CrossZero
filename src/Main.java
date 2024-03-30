c5bcbaaf6040ddab9fab6871f977bfcd0c163a6f
Reachi-Kim <reachichannel@gmail.com>
2024-03-30 14:47:43 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -6,17 +6,17 @@ public class Main
 {
     public static String name1;
     public static String  name2;
-    public static void main(String [] args)
-    {
+    public static void main(String [] args) {
         name1 = JOptionPane.showInputDialog("Введите имя Первого игрока");//окно с вводом имени Первого игрока
         name2 = JOptionPane.showInputDialog("Введите имя Второго игрока");//окно с вводом имени Второго игрока
         JFrame window = new JFrame("Крестики - Нолики");//Окно игры
-        window.setDefaultCloseOperation(WindowConstants. EXIT_ON_CLOSE);//кнопка "х" для закрытия окна
-        window.setSize(300,300);//размер окна
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);//кнопка "х" для закрытия окна
+        window.setSize(300, 300);//размер окна
         window.setLayout(new BorderLayout());//компановка
         window.setLocationRelativeTo(null);//окно по центру экрана
         window.setVisible(true);//видимость окна
         CrossZero game = new CrossZero();//объект класса игры
         window.add(game);//добавление объекта в окно
     }
+
 }
